98985a2cd7ca54aa86a04d7ece53b0e76a358353
Anna Neyzberg <aneyzberg@gmail.com>
2014-09-05 04:57:07 +0000

    updated app layout and welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Blockmarks::Application.routes.draw do
   get "welcome/about"
 
   root to: 'welcome#index'
+
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,4 +59,4 @@ Blockmarks::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
