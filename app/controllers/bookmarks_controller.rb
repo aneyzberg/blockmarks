e7df1650558ed254578d6e455ae85e6daff5b9a4
Anna Neyzberg <aneyzberg@gmail.com>
2014-09-11 06:09:29 +0000

    installed faker

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,6 +7,10 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
   end
 
+  def create
+    @bookmark = Bookmark.new(params.require(:bookmark).permit(:url))
+  end
+
   def new
     @bookmark = Bookmark.new
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require 'faker'
+
+10.times do
+
+Bookmark.create(
+  url: Faker::Internet.url
+  )
+end
+
+puts "Seed Finished"
+puts "#{Bookmark.count} urls created"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
