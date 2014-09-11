f3af37b35b7abd3120e2a1ebeb27ea40125b3412
Anna Neyzberg <aneyzberg@gmail.com>
2014-09-11 04:49:42 +0000

    started bookmark model

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,17 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def edit
+    bookmark = Bookmark.find(params[:id])
+  end
+end
