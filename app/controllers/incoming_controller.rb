cc64158c61f0ce07b67f79ba103ed24346acfa5a
Anna Neyzberg <aneyzberg@gmail.com>
2014-09-19 17:24:45 +0000

    fixing bugs

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,26 +6,39 @@ class IncomingController < ApplicationController
   def create
 
   Rails.logger.info ">>>>>>> incoming: #{params.inspect}"
+  sender = params[:sender]
+  Rails.logger.info ">>>>> sender: #{sender}"
+  url = params["stripped-text"]
+  Rails.logger.info ">>>> url #{url}"
+  subjects = params[:subject]
+  Rails.logger.info ">>>>>> subjects #{subjects}"
   
-  @user = User.find_by_email(params[:sender])
+  @user = User.find_by_email(sender)
   if @user 
+    Rails.logger.info ">>>>> user: #{@user.inspect}"
 
-    @bookmark = Bookmark.find_or_create_by_url(params["stripped-text"])
+    @bookmark = Bookmark.find_or_create_by_url(url)
+
+    Rails.logger.info ">>>>> bookmark: #{@bookmark.inspect}"
     
 
     if @bookmark.present?
-      @bookmark.users << @user   
+      @bookmark.users << @user 
+      Rails.logger.info ">>>> user added"  
 
-      topic_names = params[:subject]
+      topic_names = subjects
       topic_names.split(" ").each do |topic_name|
+        Rails.logger.info ">>>>>  current topic_name: #{topic_name}"
         topic_name = topic_name.sub(/#/, "")
         topic = Topic.find_or_create_by_name(topic_name)
+        Rails.logger.info ">>>>>> topic: #{topic.inspect}"
         @bookmark.topics << topic
+        Rails.logger.info ">>>> topic added"
       end
 
-
+      Rails.logger.info ">>>>>sending mail"
       UserMailer.bookmark_email(@user, @bookmark).deliver
-
+      Rails.logger.info "done"
        
     end
   end
